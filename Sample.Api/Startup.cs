ff22db394391edf1b1c5d4d980ce9896fea6a5ef
Sinan Caliskan <codersinan@gmail.com>
2020-05-30 01:04:18 +0300

    masstransit-moving from mediator to rabbitmq

--- Sample.Api/Startup.cs
+++ Sample.Api/Startup.cs
@@ -1,8 +1,10 @@
 using MassTransit;
+using MassTransit.Definition;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Sample.Components.Consumers;
 using Sample.Contracts.Order;
@@ -21,14 +23,19 @@ namespace Sample.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.TryAddSingleton(KebabCaseEndpointNameFormatter.Instance);
             services.AddMassTransit(cfg =>
             {
-                cfg.AddConsumer<SubmitOrderConsumer>();
+                // cfg.AddConsumer<SubmitOrderConsumer>();
+                //
+                // cfg.AddMediator();
 
-                cfg.AddMediator();
+                cfg.AddBus(provider => Bus.Factory.CreateUsingRabbitMq());
 
                 cfg.AddRequestClient<SubmitOrder>();
             });
+
+            services.AddMassTransitHostedService();
 
             services.AddOpenApiDocument(cfg => cfg.PostProcess = d => d.Info.Title = "Sample");
 
---
+++ Sample.Service/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using MassTransit;
+using MassTransit.Definition;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+using Sample.Components.Consumers;
+
+namespace Sample.Service
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+
+            var builder = new HostBuilder()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.json", true);
+                    config.AddEnvironmentVariables();
+
+                    if (args != null)
+                        config.AddCommandLine(args);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.TryAddSingleton(KebabCaseEndpointNameFormatter.Instance);
+                    services.AddMassTransit(cfg =>
+                    {
+                        cfg.AddConsumersFromNamespaceContaining<SubmitOrderConsumer>();
+
+                        cfg.AddBus(ConfigureBus);
+                    });
+
+                    services.AddHostedService<MassTransitConsoleHostedService>();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                });
+
+
+            if (isService)
+                await builder.UseWindowsService().Build().RunAsync();
+            else
+                await builder.RunConsoleAsync();
+        }
+
+        static IBusControl ConfigureBus(IRegistrationContext<IServiceProvider> context)
+        {
+            return Bus.Factory.CreateUsingRabbitMq(cfg => { cfg.ConfigureEndpoints(context); });
+        }
+    }
+}
